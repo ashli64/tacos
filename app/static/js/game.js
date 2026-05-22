969319db7be36d4a18a6de88b1f2a131b4eb4ef7
amethyst_is_cool <veronika.duvanova@gmail.com>
2026-05-22 11:49:29 -0400

    hey

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -10,14 +10,23 @@ context.canvas.height = window.innerWidth * .350;
 
 const start_screen_bg = new Image();
 start_screen_bg.src = "/static/img/start_screen_bg.png";
+const start_button = new Image();
+start_button.src = "/static/img/start_button.png";
+const tutorial_button = new Image();
+tutorial_button.src = "/static/img/tutorial_button.png";
 
-start_screen_bg.onload = function () {
+const SCALE_FACTOR = canvas.width / start_button.width;
 
-    console.log("image loaded");
-
+tutorial_button.onload = function () {
     context.drawImage(
         start_screen_bg, 0, 0, canvas.width, canvas.height
     );
-};
 
-start_screen();
+    //need to make buttons scaled with screen too
+    context.drawImage(
+        start_button, canvas.width/2 - start_button.width/2, canvas.height/2, start_button.width / SCALE_FACTOR
+    );
+    context.drawImage(
+        tutorial_button, canvas.width/2 - start_button.width/2, canvas.height/2 + tutorial_button.width/1.5
+    );
+}
