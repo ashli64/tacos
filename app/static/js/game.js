fcb75371e9f61714a2a2f8c6419920e167e3a68e
amethyst_is_cool <veronika.duvanova@gmail.com>
2026-05-21 21:07:48 -0400

    game

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,5 +1,16 @@
-int PHASE = 0
 //0 = start screen
 //1 tutorial
 //2 game
 //3 Night Complete Screen
+
+var canvas = document.getElementById('main-canvas');
+var context = canvas.getContext('2d');
+context.canvas.width = window.innerWidth * .50;
+context.canvas.height = window.innerWidth * .350;
+
+const start_screen_bg = document.getElementById("start_screen_bg");
+
+function start_screen() {
+
+
+}
