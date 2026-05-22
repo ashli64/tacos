879a6ac3d6731bad4fe7617aa687677610ce826e
Veronika Duvanova <158002997+amethyst-is-cool@users.noreply.github.com>
2026-05-22 13:19:13 -0400

    Update game.js for async image loading and scaling
    Refactor game.js to use async image loading and correct scaling.

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,13 +1,18 @@
-//0 = start screen
-//1 tutorial
-//2 game
-//3 Night Complete Screen
-
+//SETUP STUFF
 var canvas = document.getElementById('main-canvas');
 var context = canvas.getContext('2d');
 context.canvas.width = window.innerWidth * .50;
 context.canvas.height = window.innerWidth * .350;
 
+//GAME MODE
+//0 = start screen
+//1 = tutorial
+//2 - run game
+//3 - night end screen
+
+const GAME_MODE = 0;
+
+//START SCREEN IMAGES
 const start_screen_bg = new Image();
 start_screen_bg.src = "/static/img/start_screen_bg.png";
 const start_button = new Image();
@@ -15,18 +20,26 @@ start_button.src = "/static/img/start_button.png";
 const tutorial_button = new Image();
 tutorial_button.src = "/static/img/tutorial_button.png";
 
-const SCALE_FACTOR = canvas.width / start_button.width;
+//TO CREATE SCALEABLE GAME
+const SCALE_FACTOR = start_button.width / canvas.width;
 
-tutorial_button.onload = function () {
-    context.drawImage(
-        start_screen_bg, 0, 0, canvas.width, canvas.height
-    );
+async function start_screen() {
+    try {
+        const images = await Promise.all([start_screen_bg, start_button, tutorial_button].map(url => loadImage(url)));
+    
+        context.drawImage(
+            start_screen_bg, 0, 0, canvas.width, canvas.height
+        );
 
-    //need to make buttons scaled with screen too
-    context.drawImage(
-        start_button, canvas.width/2 - start_button.width/2, canvas.height/2, start_button.width / SCALE_FACTOR
-    );
-    context.drawImage(
-        tutorial_button, canvas.width/2 - start_button.width/2, canvas.height/2 + tutorial_button.width/1.5
-    );
+        context.drawImage(
+            start_button, canvas.width/2 - start_button.width/2, canvas.height/2, start_button.width / SCALE_FACTOR
+        );
+    
+        context.drawImage(
+            tutorial_button, canvas.width/2 - start_button.width/2, canvas.height/2 + tutorial_button.width/1.5
+        );
+    } catch (error) {
+        console.error("Some images may have not loaded) }
 }
+
+start_screen();
